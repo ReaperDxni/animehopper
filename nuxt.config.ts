3dfe13f7c1e960d6d2be0fa3103f232dcbf1d513
ReaperMaga <reapermaga@gmail.com>
2024-03-29 10:52:35 +0100

    Removed boilerplate, fixed prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@sidebase/nuxt-auth',
-    '@nuxtjs/tailwindcss',
-    '@bg-dev/nuxt-naiveui'
+    "@sidebase/nuxt-auth",
+    "@nuxtjs/tailwindcss",
+    "@bg-dev/nuxt-naiveui",
   ],
   typescript: {
-    shim: false
+    shim: false,
+  },
+  tailwindcss: {
+    exposeConfig: true,
   }
-})
+});
