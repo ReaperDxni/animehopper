e7e79361967825fdea66f097c9019b75b5ed15bb
ReaperMaga <reapermaga@gmail.com>
2024-03-29 11:21:51 +0100

    Added color pallet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@sidebase/nuxt-auth",
-    "@nuxtjs/tailwindcss",
-    "@bg-dev/nuxt-naiveui",
+    '@sidebase/nuxt-auth',
+    '@nuxtjs/tailwindcss',
+    '@bg-dev/nuxt-naiveui',
+    'nuxt-icon',
+    '@nuxtjs/google-fonts'
   ],
   typescript: {
-    shim: false,
+    shim: false
   },
   tailwindcss: {
     exposeConfig: true,
-  }
-});
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            backcolor: '#0A090C',
+            primary: '#D62828',
+            secondary: '#60D394'
+          }
+        }
+      }
+    }
+  },
+  devtools: { enabled: true },
+  googleFonts: { families: { Rubik: true } }
+})
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="bg-black h-screen">
-    <h1 class="text-white">
-      This is a test number 2
-    </h1>
+  <div class="bg-backcolor h-screen">
+    <Icon name="material-symbols:home" color="white" />
   </div>
 </template>
